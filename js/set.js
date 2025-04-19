12e639c7edd19ec9d03747cf7e6cb06702aeaa9c
YouQi <youqiya233@163.com>
2025-04-19 21:38:10 +0800

    修复特性和添加卡顿

--- js/set.js
+++ js/set.js
@@ -1,13 +1,22 @@
 function setMcUrl(){
-    const mcUrlFor = localStorage.getItem('mcUrl')
-    const mcUrlSet = prompt(`默认：minecraft://\n现在：${mcUrlFor}`, mcUrlFor)
-    if(mcUrlSet != null){
-      localStorage.setItem("mcUrl", mcUrlSet);
-    }
+  const mcUrlFor = localStorage.getItem('mcUrl')
+  const mcUrlSet = prompt(`默认：minecraft://\n现在：${mcUrlFor}`, mcUrlFor)
+  localStorage.setItem('mcUrl',mcUrlSet)
+  console.log(`我被改成${mcUrlSet}为启动方式了！`)
+}
+function setZeroIframe(){
+  const zeroIframe = localStorage.getItem('ZeroIframe')
+  const zeroIframeNow = prompt('URL',zeroIframe)
+  localStorage.setItem('zeroIframe',zeroIframeNow)
 }
 
 if(localStorage.getItem('mcUrl') == null){
-    localStorage.setItem('mcUrl', "minecraft://")
+  localStorage.setItem('mcUrl', "minecraft://")
+}
+if(localStorage.getItem('zeroIframe') == null){
+  localStorage.setItem('zeroIframe','cn.bing.com')
 }
 
 document.getElementById('mcUrlTip').innerText = localStorage.getItem('mcUrl')
+const zeroIframeNowUrl = localStorage.getItem('zeroIframe')
+document.getElementById('zeroIframeE').innerHTML = `<iframe src='${zeroIframeNowUrl}' weight='100%' height='300'></iframe>`
--- js/tool.js
+++ js/tool.js
@@ -1,13 +1,13 @@
 function startMinecraft(){
-    console.log('瑞典原神，启动！')
-    const startMinecraftUrl = document.createElement('a')
-    startMinecraftUrl.href = localStorage.getItem("mcUrl");
-    startMinecraftUrl.style.display = 'none'
-    document.body.appendChild(startMinecraftUrl)
-    startMinecraftUrl.click()
-    document.body.removeChild(startMinecraftUrl)
+  console.log('瑞典原神，启动！')
+  const startMinecraftUrl = document.createElement('a')
+  startMinecraftUrl.href = localStorage.getItem("mcUrl");
+  startMinecraftUrl.style.display = 'none'
+  document.body.appendChild(startMinecraftUrl)
+  startMinecraftUrl.click()
+  document.body.removeChild(startMinecraftUrl)
 
-    setTimeout(() => {
+  setTimeout(() => {
         // window.location = ""
-    }, 500)
+  }, 500)
 }
